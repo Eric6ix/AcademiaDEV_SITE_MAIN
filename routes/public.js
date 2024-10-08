6898bc6f83eef0589dae0b44240ea87d82d07a79
Eric M. F. Freitas <142802977+Eric6ix@users.noreply.github.com>
2024-10-08 14:52:12 -0300

    atualização
    eric

--- routes/public.js
+++ routes/public.js
@@ -1,12 +1,27 @@
 import express from "express";
-
-const router = express.Router()
+import bcrypt from "bcrypt";
+const router = express.Router();
 
 //Cadastro
-router.post('/cadastro', (req, res) =>{
-    const user = req.body
+router.post("/cadastro", async (req, res) => {
+  try {
+    const user = req.body;
 
-    res.status(201).json(user)
-})
+    const slat = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(user.password)
 
-export default router
+
+    const userDB = await prisma.user.create({
+      data: {
+        email: user.email,
+        name: user.name,
+        password,
+      },
+    });
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json({ message: "Erro no servidor, tente novamente" });
+  }
+});
+
+export default router;
